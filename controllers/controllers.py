4346aa07f5d66609d0751c17fd4dc29a58c6ef98
Fikri Anda <102774997+Aero98@users.noreply.github.com>
2024-09-04 22:35:37 +0700

    Update controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,27 +1,14 @@
 from odoo import http
 from odoo.http import request
+from odoo.exceptions import ValidationError
+import json
 
 class MaterialController(http.Controller):
 
-    @http.route('/api/materials', type='json', auth='public', methods=['GET'])
-    def get_materials(self, material_type=None):
-        domain = []
-        if material_type:
-            domain = [('material_type', '=', material_type)]
-        materials = request.env['material.material'].search(domain)
-        materials_list = []
-        for material in materials:
-            materials_list.append({
-                'material_code': material.material_code,
-                'material_name': material.material_name,
-                'material_type': material.material_type,
-                'material_buy_price': material.material_buy_price,
-                'related_supplier': material.related_supplier_id.name,
-            })
-        return materials_list
-
     @http.route('/api/material/<int:material_id>', type='json', auth='public', methods=['GET'])
     def get_material(self, material_id):
+        parameters = json.loads(request.httprequest.data)
+        material_id = parameters.get('id')
         material = request.env['material.material'].browse(material_id)
         return {
             'material_code': material.material_code,
@@ -31,14 +18,16 @@ class MaterialController(http.Controller):
             'related_supplier': material.related_supplier_id.name,
         }
 
-    @http.route('/api/material', type='json', auth='public', methods=['POST'])
+    @http.route('/api/material', type='json', auth='public', methods=['POST'], csrf=False)
     def create_material(self, **kwargs):
+        parameters = json.loads(request.httprequest.data)
+        # open('/opt/odoo/logs/create_material.log', 'a+').write('\n' + str(parameters))
         new_material = request.env['material.material'].create({
-            'material_code': kwargs.get('material_code'),
-            'material_name': kwargs.get('material_name'),
-            'material_type': kwargs.get('material_type'),
-            'material_buy_price': kwargs.get('material_buy_price'),
-            'related_supplier_id': kwargs.get('related_supplier_id'),
+            'material_code': parameters.get('material_code'),
+            'material_name': parameters.get('material_name'),
+            'material_type': parameters.get('material_type'),
+            'material_buy_price': parameters.get('material_buy_price'),
+            'related_supplier_id': parameters.get('related_supplier_id'),
         })
         return {
             'id': new_material.id,
@@ -47,13 +36,15 @@ class MaterialController(http.Controller):
 
     @http.route('/api/material/<int:material_id>', type='json', auth='public', methods=['PUT'])
     def update_material(self, material_id, **kwargs):
+
+        parameters = json.loads(request.httprequest.data)
         material = request.env['material.material'].browse(material_id)
         material.write({
-            'material_code': kwargs.get('material_code'),
-            'material_name': kwargs.get('material_name'),
-            'material_type': kwargs.get('material_type'),
-            'material_buy_price': kwargs.get('material_buy_price'),
-            'related_supplier_id': kwargs.get('related_supplier_id'),
+            'material_code': parameters.get('material_code'),
+            'material_name': parameters.get('material_name'),
+            'material_type': parameters.get('material_type'),
+            'material_buy_price': parameters.get('material_buy_price'),
+            'related_supplier_id': parameters.get('related_supplier_id'),
         })
         return {
             'message': 'Material updated successfully!'
@@ -61,6 +52,9 @@ class MaterialController(http.Controller):
 
     @http.route('/api/material/<int:material_id>', type='json', auth='public', methods=['DELETE'])
     def delete_material(self, material_id):
+        parameters = json.loads(request.httprequest.data)
+        material_id = parameters.get('id')
+        open('/opt/odoo/logs/create_material.log', 'a+').write('\n' + str(parameters))
         material = request.env['material.material'].browse(material_id)
         material.unlink()
         return {
